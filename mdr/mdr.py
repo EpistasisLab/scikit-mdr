f198fd2549946d5cc4fce3a25973e99aa8f41cf8
Randy Olson <rso@randalolson.com>
2017-01-06 15:28:22 -0500

    Small code cleanup

--- mdr/mdr.py
+++ mdr/mdr.py
@@ -47,16 +47,16 @@ class MDR(BaseEstimator):
         self.tie_break = tie_break
         self.default_label = default_label
         self.class_fraction = 0.
-        self.feature_map = defaultdict(lambda: default_label)
+        self.feature_map = None
 
-    def fit(self, features, classes):
+    def fit(self, features, class_labels):
         """Constructs the MDR feature map from the provided training data
 
         Parameters
         ----------
         features: array-like {n_samples, n_features}
             Feature matrix
-        classes: array-like {n_samples}
+        class_labels: array-like {n_samples}
             List of class labels for prediction
 
         Returns
@@ -64,8 +64,9 @@ class MDR(BaseEstimator):
         None
 
         """
-        self.unique_labels = sorted(np.unique(classes))
-        self.class_fraction = float(sum(classes == self.unique_labels[0])) / (classes.size) # Only applies to binary classification 
+        self.feature_map = defaultdict(lambda: default_label)
+        self.unique_labels = sorted(np.unique(class_labels))
+        self.class_fraction = float(sum(class_labels == self.unique_labels[0])) / (class_labels.size) # Only applies to binary classification 
         num_classes = len(self.unique_labels) # Count all the unique values of classes
 
         if num_classes != 2:
@@ -75,7 +76,7 @@ class MDR(BaseEstimator):
 
         for row_i in range(features.shape[0]):
             feature_instance = tuple(features[row_i])
-            self.class_count_matrix[feature_instance][classes[row_i]] += 1
+            self.class_count_matrix[feature_instance][class_labels[row_i]] += 1
 
         for feature_instance in self.class_count_matrix:
             counts = self.class_count_matrix[feature_instance]
@@ -101,6 +102,8 @@ class MDR(BaseEstimator):
             Constructed features from the provided feature matrix
 
         """
+        if self.feature_map is None:
+            raise ValueError('The MDR model must be fit before transform can be called')
         new_feature = np.zeros(features.shape[0], dtype=np.int)
 
         for row_i in range(features.shape[0]):
@@ -109,14 +112,14 @@ class MDR(BaseEstimator):
 
         return new_feature.reshape(features.shape[0], 1)
 
-    def fit_transform(self, features, classes):
+    def fit_transform(self, features, class_labels):
         """Convenience function that fits the provided data then constructs a new feature from the provided features
 
         Parameters
         ----------
         features: array-like {n_samples, n_features}
             Feature matrix
-        classes: array-like {n_samples}
+        class_labels: array-like {n_samples}
             List of true class labels
 
         Returns
@@ -125,7 +128,7 @@ class MDR(BaseEstimator):
             Constructed features from the provided feature matrix
 
         """
-        self.fit(features, classes)
+        self.fit(features, class_labels)
         return self.transform(features)
 
     def predict(self, features):
@@ -142,6 +145,8 @@ class MDR(BaseEstimator):
             Constructed features from the provided feature matrix
 
         """
+        if self.feature_map is None:
+            raise ValueError('The MDR model must be fit before predict can be called')
         new_feature = np.zeros(features.shape[0], dtype=np.int)
 
         for row_i in range(features.shape[0]):
@@ -150,14 +155,14 @@ class MDR(BaseEstimator):
 
         return new_feature
 
-    def fit_predict(self, features, classes):
+    def fit_predict(self, features, class_labels):
         """Convenience function that fits the provided data then constructs a new feature from the provided features
 
         Parameters
         ----------
         features: array-like {n_samples, n_features}
             Feature matrix
-        classes: array-like {n_samples}
+        class_labels: array-like {n_samples}
             List of true class labels
 
         Returns
@@ -166,17 +171,17 @@ class MDR(BaseEstimator):
             Constructed features from the provided feature matrix
 
         """
-        self.fit(features, classes)
+        self.fit(features, class_labels)
         return self.predict(features)
 
-    def score(self, features, classes, scoring_function=None, **scoring_function_kwargs):
+    def score(self, features, class_labels, scoring_function=None, **scoring_function_kwargs):
         """Estimates the accuracy of the predictions from the constructed feature
 
         Parameters
         ----------
         features: array-like {n_samples, n_features}
             Feature matrix to predict from
-        classes: array-like {n_samples}
+        class_labels: array-like {n_samples}
             List of true class labels
 
         Returns
@@ -185,13 +190,12 @@ class MDR(BaseEstimator):
             The estimated accuracy based on the constructed feature
 
         """
-        if len(self.feature_map) == 0:
-            raise ValueError('The MDR model must be fit before score() can be called')
+        if self.feature_map is None:
+            raise ValueError('The MDR model must be fit before score can be called')
 
         new_feature = self.transform(features)
 
         if scoring_function is None:
-            return accuracy_score(classes, new_feature)
+            return accuracy_score(class_labels, new_feature)
         else:
-            return scoring_function(classes, new_feature, **scoring_function_kwargs)
-
+            return scoring_function(class_labels, new_feature, **scoring_function_kwargs)
