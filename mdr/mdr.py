a6aabcf51558066694063f2adae95e1cb0d38816
Randy Olson <rso@randalolson.com>
2017-11-29 10:40:27 -0500

    Update license statement to include authors

--- mdr/mdr.py
+++ mdr/mdr.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """
+scikit-MDR was primarily developed at the University of Pennsylvania by:
+    - Randal S. Olson (rso@randalolson.com)
+    - Tuan Nguyen (tnguyen4@swarthmore.edu)
+    - and many more generous open source contributors
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 and associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
